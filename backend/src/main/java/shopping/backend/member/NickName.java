b8bf143396c7451033edd2d7ada599915a048e5f
A7024E <rudqls6671@naver.com>
2025-11-13 17:56:28 +0900

    feat(Member) : nickName 객체 추가

---
+++ backend/src/main/java/shopping/backend/member/NickName.java
@@ -0,0 +1,13 @@
+package shopping.backend.member;
+
+public class NickName {
+    private String NickName;
+
+    public NickName(String nickName) {
+        NickName = nickName;
+    }
+
+    public String getNickName() {
+        return NickName;
+    }
+}
