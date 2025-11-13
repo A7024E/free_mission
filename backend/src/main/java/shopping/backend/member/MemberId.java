83d1e2ad58e85e28fd1ef49b253676d9a8f2b8c6
A7024E <rudqls6671@naver.com>
2025-11-13 17:56:00 +0900

    feat(Member) : memberId 객체 추가

---
+++ backend/src/main/java/shopping/backend/member/MemberId.java
@@ -0,0 +1,13 @@
+package shopping.backend.member;
+
+public class MemberId {
+    private String id;
+
+    public MemberId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
