31afab6f82ea2db171eef081cea0a3bd0c17f53c
A7024E <rudqls6671@naver.com>
2025-11-13 20:35:01 +0900

    feat(member): MemberService에 회원 가입·로그인 처리 로직 및 트랜잭션 설정 추가

--- backend/src/main/java/shopping/backend/member/service/MemberService.java
+++ backend/src/main/java/shopping/backend/member/service/MemberService.java
@@ -1,5 +1,7 @@
 package shopping.backend.member.service;
 
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 import shopping.backend.member.Gender;
 import shopping.backend.member.Member;
 import shopping.backend.member.MemberId;
@@ -8,6 +10,8 @@ import shopping.backend.member.NickName;
 import shopping.backend.member.Password;
 import shopping.backend.member.dto.MemberJoinRequest;
 
+@Service
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
@@ -16,11 +20,11 @@ public class MemberService {
     }
 
     public void join(MemberJoinRequest memberJoinRequest) {
-        if(memberRepository.existsByNickName(new NickName(memberJoinRequest.nickName()))){
+        if (memberRepository.existsByNickName(new NickName(memberJoinRequest.nickName()))) {
             throw new IllegalArgumentException("중복된 닉네임이 존재합니다");
         }
 
-        if(memberRepository.findById(new MemberId(memberJoinRequest.id())).isPresent()){
+        if (memberRepository.findById(new MemberId(memberJoinRequest.id())).isPresent()) {
             throw new IllegalArgumentException("중복된 아이디가 존재합니다");
         }
 
@@ -32,4 +36,21 @@ public class MemberService {
         );
         memberRepository.save(member);
     }
+
+    @Transactional()
+    public Member login(MemberJoinRequest request){
+        MemberId memberId = new MemberId(request.id());
+        Password password = new Password(request.password());
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+        if(isPasswordMatch(request, password)){
+            throw new IllegalArgumentException("패스워드가 틀립니다 다시 확인해주세요");
+        }
+        return member;
+    }
+
+    private static boolean isPasswordMatch(MemberJoinRequest request, Password password) {
+        return password.isSame(request.password());
+    }
+
 }
