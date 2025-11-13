64b474b4d7171ab3f9744ec52e1c590738f7925d
A7024E <rudqls6671@naver.com>
2025-11-13 19:06:34 +0900

    feat: Member 엔티티 추가 (EmbeddedId, Embedded, EnumType.STRING 적용)

--- backend/src/main/java/shopping/backend/member/Member.java
+++ backend/src/main/java/shopping/backend/member/Member.java
@@ -1,11 +1,26 @@
 package shopping.backend.member;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "members")
 public class Member {
 
+    @EmbeddedId
     private MemberId id;
+    @Embedded
     private Password password;
+    @Embedded
     private NickName nickName;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    protected Member() {}
 
     public Member(MemberId id, Password password, NickName nickName, Gender gender) {
         this.id = id;
@@ -13,5 +28,7 @@ public class Member {
         this.nickName = nickName;
         this.gender = gender;
     }
+
+
 
 }
