fb3f006c65ec69117c8dec56c01dd72e9c249e45
A7024E <rudqls6671@naver.com>
2025-11-13 17:56:12 +0900

    feat(Member) : gender 객체 추가

---
+++ backend/src/main/java/shopping/backend/member/Gender.java
@@ -0,0 +1,13 @@
+package shopping.backend.member;
+
+public class Gender {
+    private String gender;
+
+    public Gender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
