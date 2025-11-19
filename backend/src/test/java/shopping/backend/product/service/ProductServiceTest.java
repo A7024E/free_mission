2a21f3500cdf95428034fa61ed49bb7e13412ff5
A7024E <rudqls6671@naver.com>
2025-11-20 04:39:55 +0900

    test: 존재하지 않는 단품 상품의 조회가 실패하는지 테스트

--- backend/src/test/java/shopping/backend/product/service/ProductServiceTest.java
+++ backend/src/test/java/shopping/backend/product/service/ProductServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import shopping.backend.product.dto.ProductListResponse;
+import shopping.backend.product.dto.ProductPageResponse;
 import shopping.backend.product.model.Category;
 import shopping.backend.product.model.Description;
 import shopping.backend.product.model.Price;
@@ -52,6 +54,34 @@ class ProductServiceTest {
         assertEquals(1, list.size());
         assertEquals("3D 고양이", list.get(0).name());
         verify(productRepository, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("상품 단건 조회 성공")
+    void findOne_success() {
+        // given
+        Product p = sampleProduct();
+        when(productRepository.findById(1L)).thenReturn(Optional.of(p));
+
+        // when
+        ProductPageResponse response = productService.findOne(1L);
+
+        // then
+        assertEquals("3D 고양이", response.name());
+        assertEquals(5000, response.price());
+        assertEquals("고양이는 귀엽다", response.description());
+
+        verify(productRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("상품 단건 조회 실패 - 존재하지 않음")
+    void findOne_fail_not_found() {
+        // given
+        when(productRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // expect
+        assertThrows(IllegalArgumentException.class, () -> productService.findOne(1L));
     }
 
 }
